3456d7db05bf55ae9bf0d8f4205b01125705746f
Xavier <castelb9822@gmail.com>
2026-03-26 15:55:37 +0100

    à tester

--- blackjack.py
+++ blackjack.py
@@ -2,89 +2,69 @@
 import random
 
 def blackjack(argent2):
-    carte = [
+    carte = [   #liste des cartes, uniquement leurs valeurs le reste est inutile
         1,2,3,4,5,6,7,8,9,10,10,10,10,
         1,2,3,4,5,6,7,8,9,10,10,10,10,
         1,2,3,4,5,6,7,8,9,10,10,10,10,
         1,2,3,4,5,6,7,8,9,10,10,10,10
         ]
 
-    nb = random.randint(0,51)
+    nb = random.randint(0,51) #tire une carte au hasard 
     cartejouer = carte[nb]
-    carte.pop(nb)
+    carte.pop(nb) #retire la carte déjà tiré du jeu
     x = 1
     print(cartejouer)
     hit = cartejouer
     stay = 1
     lost = 0
     win = 0
-    while True:
-        choix = input("hit or stay")
+
+    while True: #boucle du jeu entier
+        choix = input("tirer or rester \n") #choix
         print(choix)
-        if choix == "hit":
-            print("hit")
-            nb = random.randint(1,51-x)
+        if choix.lower() == "tirer":
+            print("tirer")
+            nb = random.randint(1,51-x) 
             cartejouer = carte[nb]
             print("dealer :")
             print(cartejouer)
             x += 1
             carte.pop(nb)
             hit += cartejouer
-            print("you :")
+            print("vous :")
             print(hit)
-            if hit > 20:
+            if hit > 20: #teste si la carte après le "tirer" reste sous les 21
                 break
-        elif choix == "stay":
+        elif choix == "rester":
             stay = 1
-            print(f"you stay at {hit}")
-            nb = random.randint(1, 51-x)
+            print(f"vous restez à {hit}")
+            nb = random.randint(1, 51-x) #sors une dernière carte
             cartejouer = carte[nb]
             print("dealer :")
             print(cartejouer)
             hit += cartejouer
-            print("you :")
+            print("vous :")
             print(hit)
-            if hit < 22:
+            if hit < 22: #teste si le "rester" compte ou non
                 lost = 1
             elif hit > 21:
                 win = 1
             break
 
     if hit < 21 and lost != 1 or win == 1:
-        print(f"you win")
-        print(f"your money goes from {argent2} to {argent2*2}")
+        print(f"vous gagnez")
+        print(f"votre argent passe de {argent2} à {argent2*2}")
         argent2 =  argent2*2
     elif hit > 21 or lost == 1:
-        print("too bad you lose your money")
+        print("Dommage, vous perdez tout...")
         argent2 = 0
     elif hit == 21:
         print("Blackjack !")
-        print(f"Jackpot! your money goes from {argent2} to {argent2*3}")
+        print(f"Jackpot! votre argent passe de {argent2} à {argent2*3}")
         argent2 = argent2*3
     return argent2
 
-def menu():
-    end = 0
-    mise = 0
-    argent = int(input("your total money"))
-    while end != 1:
-        while True:
-            mise = int(input("what's  your bet ?"))
-            if mise > argent:
-                print("too poor")
-            else:
-                break
-        argent = argent - mise
-        print(f"bet placed for {mise}$")
-        argent = argent + blackjack(mise)
-        if argent < 1:
-            end = 1
-        if end != 1:
-            a = input("Do you want do continu ? yes/no")
-            if a == "no":
-                end = 1
-    return argent
-print(f"You end your game with {menu()}$ in your pockets")
+print(blackjack(10))
 
 
 
