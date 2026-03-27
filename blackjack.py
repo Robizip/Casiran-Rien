1c65c11e81848dc2ff51db48a3606989c1b1ef3a
Xavier <castelb9822@gmail.com>
2026-03-27 11:39:36 +0100

    100% bon

--- blackjack.py
+++ blackjack.py
@@ -20,27 +20,28 @@ def blackjack(argent2):
     win = 0
 
     while True: #boucle du jeu entier
-        choix = input("tirer or rester \n") #choix
+        choix = input("tire or reste \n") #choix
         print(choix)
-        if choix.lower() == "tirer":
-            print("tirer")
+        if choix.lower() == "tire":
+            print("vous tirez")
             nb = random.randint(1,51-x) 
             cartejouer = carte[nb]
-            print("dealer :")
+            print("\ncroupier :")
             print(cartejouer)
             x += 1
             carte.pop(nb)
             hit += cartejouer
-            print("vous :")
+            print("\nvous :")
             print(hit)
-            if hit > 20: #teste si la carte après le "tirer" reste sous les 21
+            print("\n")
+            if hit > 20: #teste si la carte après le "tirer" compte ou non
                 break
-        elif choix == "rester":
+        elif choix == "reste":
             stay = 1
             print(f"vous restez à {hit}")
             nb = random.randint(1, 51-x) #sors une dernière carte
             cartejouer = carte[nb]
-            print("dealer :")
+            print("croupier :")
             print(cartejouer)
             hit += cartejouer
             print("vous :")
@@ -51,18 +52,18 @@ def blackjack(argent2):
                 win = 1
             break
 
-    if hit < 21 and lost != 1 or win == 1:
-        print(f"vous gagnez")
+    if hit < 21 and lost != 1 or win == 1: #teste si le joueur a gagner en testant : que le total fasse moins que 21
+        print(f"vous gagnez")              #et qu'il n'ai pas perdu, ou alors qu'il ai gagné en étant "rester" ou "stay"
         print(f"votre argent passe de {argent2} à {argent2*2}")
         argent2 =  argent2*2
-    elif hit > 21 or lost == 1:
+    elif hit > 21 or lost == 1: # si le total dépasse 21 ou que le joueur a perdu il perd tout
         print("Dommage, vous perdez tout...")
         argent2 = 0
-    elif hit == 21:
+    elif hit == 21:  #teste si le joueur a gagné (grace aux autres testes) si le total est égale à exactement 21 pour le jackpot 
         print("Blackjack !")
         print(f"Jackpot! votre argent passe de {argent2} à {argent2*3}")
         argent2 = argent2*3
-    return argent2
+    return argent2 #renvoie l'argent gagné ou perdu à la fin du jeu
 
 print(blackjack(10))
 
