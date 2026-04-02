0045f85c93a649fccdeef09dcbc5e7567d7d43f3
Xavier <castelb9822@gmail.com>
2026-04-02 15:00:51 +0200

    fr + temps

--- roulette.py
+++ roulette.py
@@ -1,6 +1,7 @@
 # Créé par Benjamin Castel, le 17/09/2025 en Python 3.7
 import random
-argent = int(input("total money"))
+import time
+argent = int(input("argent totale"))
 
 
 def roulette(argent):
@@ -11,15 +12,14 @@ def roulette(argent):
 
     while endbet < 1:
         while True:
-            print("Choose a number (0 to 37) or a color (red or black)")
-            print("betting on a single number has a pay of 35:1 \nbetting on a color is a 1:1")
-            print(f"You have {argent}$")
-            choix = input("Place a bet (the category): ").strip().lower()
-
-
-            if choix == "red" or choix == "black":
-                bet.append(choix)
-                break
+            time.sleep(1)
+            print("choisissez un nombre (0 to 37) ou une couleur rouge ou noir)")
+            time.sleep(1)
+            print("parier sur un nombre vaut 35 pour 1 misé \nparier sur une couleur vaut 2 pour 1 misé")
+            time.sleep(1)
+            print(f"vous avez {argent}$")
+            time.sleep(1)
+            choix = input("Placez un parie (la catégorie): ").strip().lower()
 
 
             if choix.isdigit():
@@ -28,75 +28,64 @@ def roulette(argent):
                     bet.append(numero)
                     break
                 else:
-                    print("Number must be between 0 and 37.")
-            else:
-                print("Invalid input. Please enter a number between 0-37 or 'red'/'black'.")
+                    print("Le nombre doit être entre 0 et 37.")
+            
+            choix = choix.lower()
+            if choix == "rouge" or choix == "noir":
+                bet.append(choix)
+                break
 
-            print("wrong input, try again")
-        print(f"You have {argent}$")
+            else:
+                print("Parie invalide. veillez entreer un nombre de 0-37 ou 'rouge'/'noir'.")
+
+            print("Parie invalide, veillez réessayer")
+        print(f"\n\nVous avez {argent}$")
         while True:
             try:
-                bb = int(input("How much do you want to bet on that? "))
+                time.sleep(1)
+                bb = int(input("Combien voulez vous parier ? "))
                 if bb <= argent and bb > 0:
                     argent -= bb
                     bet.append(bb)
                     break
                 else:
-                    print("You can't bet more than you have or a non-positive amount.")
-                    print("The house doesn't make credit.")
+                    print("La maison de fait pas crédit.")
+                    time.sleep(1)
+                    print("Revenez quand vous un petit peu plus... riche.")
             except ValueError:
-                print("Please enter a valid number.")
+                print("Veillez enter un nombre valide.")
         if argent == 0:
             endbet = 1
         if argent > 0:
-            a = input("Do you want to place another bet ? yes/no")
-            if a == "no":
+            time.sleep(1)
+            a = input("Voulez vous placer un autre parie ? oui/non")
+            if a.lower() == "non":
                 endbet = 1
 
     number = random.randint(0,37)
 
     if number == 0:
-        color = "green"
+        color = "vert"
     elif number in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]:
-        color = "red"
+        color = "rouge"
     else:
-        color = "black"
-    print(color)
-    for z in range(0, len(bet), 2):  # <- correction ici
+        color = "noir"
+    print(f"{number} {color}")
+    for z in range(0, len(bet), 2):  
         if bet[z] == number:
-            print(f"Congrats, you win with a number bet with the number {bet[z]}\n"
-            "your bet money ({bet[z+1]}), with a 35:1 rule, goes up to {bet[z+1]*36}")
+            time.sleep(1)
+            print(f"Bravo, vous gagnez grâce à un nombre {bet[z]}\n"
+            "votre argent parié ({bet[z+1]}), avec une côte de 35 pour 1, monte à {bet[z+1]*36}")
             argent += bet[z+1]*36
-        elif str(bet[z]) == color:  # inutile de caster deux fois
-            print(f"Congrats, you win with a color bet with the color {color}\n"
-            "your bet money ({bet[z+1]}), with a 1:1 rule, goes up to {bet[z+1]*2}")
+        elif str(bet[z]) == color: 
+            time.sleep(1)
+            print(f"Bravo vous gagnez grâce à une couleur {color}\n"
+            "votre argent parié ({bet[z+1]}), avec une côte de 2 pour 1, monte à {bet[z+1]*2}")
             argent += bet[z+1]*2
 
-
-    print(f"Your money went from: {startmoney}, to {argent}.")
+    time.sleep(2)
+    print(f"\n\n\nEn tout, votre argent est allé de {startmoney}€, à {argent}€.")
     return argent
 
 
-
-end = 0
-play = roulette(argent)
-if play > 0:
-    while end < 1:
-        d = input("do you want to play again ? yes/no")
-        if d == "yes":
-            print("good luck")
-
-            while True:
-                dd = int(input("how much of your money do you wana re bet ?"))
-                if dd > argent:
-                    print("The house doesn't make credits")
-                elif dd > 0:
-                    argent = dd
-                    play -= argent
-                    play += roulette(argent)
-        elif d == "no":
-            end = 1
-        else:
-            print("incorrect input, please try again")
-else:
-    print("Im sorry you can't continu without any money to bet")
+print(roulette(10))
