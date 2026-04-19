86427a65623e8c932a515d97873a3c515daff945
Mathis-byte <mathisnougarede@gmail.com>
2026-04-19 10:20:40 +0200

    J'ai rajouté les états "connecter" et "créer", on peut y rentrer du texte mtn

--- Casino.py
+++ Casino.py
@@ -37,8 +37,19 @@ button_roulette = pygame.Rect(300, 440, 300, 80)
 button_bp = pygame.Rect(300, 560, 300, 80)
 button_bm = pygame.Rect(300, 680, 300, 80)
 button_de = pygame.Rect(300, 800, 300, 80)
+input_pseudo = pygame.Rect(200, 200, 500, 40)
+input_mdp = pygame.Rect(200, 260, 500, 40)
+input_nom = pygame.Rect(200, 320, 500, 40)
+input_prenom = pygame.Rect(200, 380, 500, 40)
 
+# Variable pour le menu, la connexion et la création de/des comptes
+txt_vide = ""
 etat = "menu_principal"
+champ_actif = None
+pseudo = ""
+mot_de_passe = ""
+nom = ""
+prenom = ""
 
 running = True
 while running:
@@ -49,6 +60,10 @@ while running:
             if etat == "menu_principal":
                 if button_menu.collidepoint(event.pos):
                     etat = "menu_jeux"
+                elif button_connect.collidepoint(event.pos):
+                    etat = "connexion"
+                elif button_create.collidepoint(event.pos):
+                    etat = "creation"
             elif etat == "menu_jeux":
                 if button_loto.collidepoint(event.pos):
                     loto_run(fenetre)
@@ -64,10 +79,44 @@ while running:
                     machine_sous_run(fenetre)
                 elif button_de.collidepoint(event.pos):
                     sim_de_run(fenetre)
+            elif etat == "connexion":
+                if input_pseudo.collidepoint(event.pos):
+                    champ_actif = "pseudo"
+                elif input_mdp.collidepoint(event.pos):
+                    champ_actif = "mdp"
+
+            elif etat == "creation":
+                if input_nom.collidepoint(event.pos):
+                    champ_actif = "nom"
+                elif input_prenom.collidepoint(event.pos):
+                    champ_actif = "prenom"
+                elif input_pseudo.collidepoint(event.pos):
+                    champ_actif = "pseudo"
+                elif input_mdp.collidepoint(event.pos):
+                    champ_actif = "mdp"
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 etat = "menu_principal"
+            elif etat in ["connexion", "creation"] and champ_actif:
+                if event.key == pygame.K_BACKSPACE:
+                    if champ_actif == "pseudo":
+                        pseudo = pseudo[:-1]
+                    elif champ_actif == "mdp":
+                        mot_de_passe = mot_de_passe[:-1]
+                    elif champ_actif == "nom":
+                        nom = nom[:-1]
+                    elif champ_actif == "prenom":
+                        prenom = prenom[:-1]
+                elif event.unicode:
+                    if champ_actif == "pseudo":
+                        pseudo += event.unicode
+                    elif champ_actif == "mdp":
+                        mot_de_passe += event.unicode
+                    elif champ_actif == "nom":
+                        nom += event.unicode
+                    elif champ_actif == "prenom":
+                        prenom += event.unicode
     fenetre.blit(fond, (0, 0))
     if etat == "menu_principal":
         pygame.draw.rect(fenetre, BLUE, button_menu)
@@ -76,6 +125,28 @@ while running:
         fenetre.blit(font.render("Se connecter", True, WHITE), (button_connect.x + 20, button_connect.y + 15))
         fenetre.blit(font.render("Créer un compte", True, WHITE), (button_create.x + 5, button_create.y + 15))
         fenetre.blit(font.render("Accéder aux jeux", True, WHITE), (button_menu.x + 50, button_menu.y + 25))
+
+    elif etat == "connexion":
+        pygame.draw.rect(fenetre, WHITE, input_pseudo, 2)
+        pygame.draw.rect(fenetre, WHITE, input_mdp, 2)
+        fenetre.blit(font.render("Pseudo :", True, WHITE), (200, 170))
+        fenetre.blit(font.render("Mot de passe :", True, WHITE), (200, 230))
+        fenetre.blit(font.render(pseudo, True, DARK), (210, 205))
+        fenetre.blit(font.render("*" * len(mot_de_passe), True, DARK), (210, 265))
+
+    elif etat == "creation":
+        pygame.draw.rect(fenetre, WHITE, input_nom, 2)
+        pygame.draw.rect(fenetre, WHITE, input_prenom, 2)
+        pygame.draw.rect(fenetre, WHITE, input_pseudo, 2)
+        pygame.draw.rect(fenetre, WHITE, input_mdp, 2)
+        fenetre.blit(font.render("Nom :", True, WHITE), (200, 290))
+        fenetre.blit(font.render("Prénom :", True, WHITE), (200, 350))
+        fenetre.blit(font.render("Pseudo :", True, WHITE), (200, 170))
+        fenetre.blit(font.render("Mot de passe :", True, WHITE), (200, 230))
+        fenetre.blit(font.render(nom, True, DARK), (210, 325))
+        fenetre.blit(font.render(prenom, True, DARK), (210, 385))
+        fenetre.blit(font.render(pseudo, True, DARK), (210, 205))
+        fenetre.blit(font.render("*" * len(mot_de_passe), True, DARK), (210, 265))
 
     
     elif etat == "menu_jeux":
